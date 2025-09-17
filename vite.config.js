edba92d102369fee8fcfa5d957a94d684a374675
Anna Ruggeri <ruggeriannaclara@gmail.com>
2025-09-16 21:19:17 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "./",                 
-  plugins: [react(),tailwindcss()],
-
-  
+  // Na Vercel pode ser "/"; manter essa lógica ajuda abrir dist localmente também
+  base: process.env.VERCEL ? "/" : "./",
+  plugins: [react()],
+  server: { strictPort: false, open: true },
+  preview: { strictPort: false, open: true },
+  build: { outDir: "dist", emptyOutDir: true }
 });
-
